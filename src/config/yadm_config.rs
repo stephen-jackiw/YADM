809dfccc2676be548dfe217b99ee25e8df0fa63f
six <mtangradi5@gmail.com>
2026-03-30 10:55:37 -0400

    single files
    new files

--- src/config/yadm_config.rs
+++ src/config/yadm_config.rs
@@ -7,15 +7,32 @@ pub struct YadmConfig {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FileConfig {
+    // directories for single apps or features
     pub config_directories: Vec<String>,
+    // directories for many apps
     pub base_config_directories: Vec<String>,
+    // for adhoc config file that doesnt have its own directory
+    pub single_files: Vec<String>,
 }
 
 impl Default for FileConfig {
     fn default() -> Self {
         Self {
-            config_directories: vec!["/etc/ssh".into()],
-            base_config_directories: vec!["~/.config".into()],
+            config_directories: vec![
+                "/etc/ssh".into(),
+                "/lib/postgresql".into(),
+                "/etc/fish".into(),
+            ],
+            base_config_directories: vec![
+                "~/.config".into(),
+                "~/.local/state".into(),
+                "~/.local/share".into(),
+            ],
+            single_files: vec![
+                "~/.m2/settings.xml".into(),
+                "~/.npmrc".into(),
+                "~/.zshrc".into(),
+            ],
         }
     }
 }
