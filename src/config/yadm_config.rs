032ae0e440b910b84cdd2d52166af1d48ba6a018
six <mtangradi5@gmail.com>
2026-03-31 09:58:33 -0400

    show files in directories; exclude files

--- src/config/yadm_config.rs
+++ src/config/yadm_config.rs
@@ -1,4 +1,6 @@
+use globwalk::GlobWalkerBuilder;
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct YadmConfig {
@@ -7,12 +9,11 @@ pub struct YadmConfig {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FileConfig {
-    // directories for single apps or features
     pub config_directories: Vec<String>,
-    // directories for many apps
     pub base_config_directories: Vec<String>,
-    // for adhoc config file that doesnt have its own directory
     pub single_files: Vec<String>,
+    pub excluded_file_types: Vec<String>,
+    pub excluded_directories: Vec<String>,
 }
 
 impl Default for FileConfig {
@@ -25,14 +26,74 @@ impl Default for FileConfig {
             ],
             base_config_directories: vec![
                 "~/.config".into(),
-                "~/.local/state".into(),
-                "~/.local/share".into(),
+                // "~/.local/state".into(),
+                // "~/.local/share".into(),
             ],
             single_files: vec![
                 "~/.m2/settings.xml".into(),
                 "~/.npmrc".into(),
                 "~/.zshrc".into(),
             ],
+            excluded_file_types: vec![".log".into(), ".bin".into(), ".pyi".into()],
+            excluded_directories: vec![
+                "~/.config/coc".into(),
+                "~/.config/discord".into(),
+                "~/.config/browsh".into(),
+            ],
         }
+    }
+}
+
+impl FileConfig {
+    fn expand(path: &str) -> PathBuf {
+        shellexpand::tilde(path).to_string().into()
+    }
+
+    pub fn load_matching(&self) -> Vec<PathBuf> {
+        let mut results = Vec::new();
+        // let patterns = ["**/*.conf", "**/*.toml"];
+        let patterns = ["**/*"];
+
+        for dir in &self.config_directories {
+            let dir = Self::expand(dir);
+
+            if !dir.exists() {
+                continue;
+            }
+
+            let walker = GlobWalkerBuilder::from_patterns(&dir, &patterns)
+                .build()
+                .unwrap();
+
+            for entry in walker.filter_map(Result::ok) {
+                results.push(entry.path().to_path_buf());
+            }
+        }
+
+        for base_dir in &self.base_config_directories {
+            let base_dir = Self::expand(base_dir);
+
+            if !base_dir.exists() {
+                continue;
+            }
+
+            let walker = GlobWalkerBuilder::from_patterns(&base_dir, &patterns)
+                .build()
+                .unwrap();
+
+            for entry in walker.filter_map(Result::ok) {
+                results.push(entry.path().to_path_buf());
+            }
+        }
+
+        for file in &self.single_files {
+            let file = Self::expand(file);
+
+            if file.exists() {
+                results.push(file);
+            }
+        }
+
+        results
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg: YadmConfig = confy::load("yadm", Some("yadm"))?;
     dbg!(&cfg);
 
+    let files = cfg.file.load_matching();
+
+    dbg!(files);
+
     Ok(())
 }
